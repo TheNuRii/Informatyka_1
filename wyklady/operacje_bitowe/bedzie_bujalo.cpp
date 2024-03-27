9b5316d289451f6a29a80641d917f155cbd51f3f
TheNuRii <maciekglow0@gmail.com>
2024-03-27 12:16:57 +0100

    sito

--- wyklady/operacje_bitowe/bedzie_bujalo.cpp
+++ wyklady/operacje_bitowe/bedzie_bujalo.cpp
@@ -9,5 +9,24 @@ int main(){
     // Reprezentacja w systemie 8
     int mask = 010;
     // w systemie dziesi
+
+    {
+        // Sito Eastotrnesa
+        int range;
+        cin >> range;
+        unsigned int sieve = 0xFFFFFFFC; // 0 -> nie jest liczbą pierwszą
+        int prime = 2;
+        while (prime * prime <= range){
+            for (int multiple = 2 * prime; multiple <= range; multiple += prime)
+                sieve &= ~(1 << multiple);
+
+            do{         // znajdz kolejna liczbe pierwsza
+                prime ++;
+            }   while ( !(sieve & (1 << prime)) );
+        }
+        for (int i = 0; i < 32; i++)
+            if (sieve & (1 << i)) cout << i << " ";
+
+    }
     return 0;
 }
